c9e97ef01499babf4d1186b583c57e9b4400115b
Piper Merriam <pipermerriam@gmail.com>
2015-09-09 23:21:39 -0600

    get some tests passing

--- tests/test_check_if_call_successful_for_failed_call.py
+++ tests/test_check_if_call_successful_for_failed_call.py
@@ -1,12 +1,15 @@
+from populus.utils import wait_for_transaction
+
+
 deploy_max_wait = 10
-deploy_max_retries = 3
 
 
 def test_check_if_call_successful_for_failed_call(geth_node, deployed_contracts):
     alarm = deployed_contracts.Alarm
     client_contract = deployed_contracts.Fails
 
-    client_contract.scheduleIt.sendTransaction(alarm._meta.address)
+    txn_hash = client_contract.scheduleIt.sendTransaction(alarm._meta.address)
+    wait_for_transaction(client_contract._meta.rpc_client, txn_hash)
 
     assert client_contract.value.call() is False
 
@@ -16,8 +19,9 @@ def test_check_if_call_successful_for_failed_call(geth_node, deployed_contracts)
     assert alarm.checkIfCalled.call(callKey) is False
     assert alarm.checkIfSuccess.call(callKey) is False
 
-    alarm.doCall.sendTransaction(callKey)
+    call_txn_hash = alarm.doCall.sendTransaction(callKey)
+    wait_for_transaction(alarm._meta.rpc_client, call_txn_hash)
 
-    assert client_contract.value.call() is True
+    assert client_contract.value.call() is False
     assert alarm.checkIfCalled.call(callKey) is True
     assert alarm.checkIfSuccess.call(callKey) is False

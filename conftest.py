5d02f714227caca824314d277d7711a2ca8d572a
Piper Merriam <pipermerriam@gmail.com>
2016-09-01 16:03:40 -0600

    1 test passes

--- conftest.py
+++ conftest.py
@@ -1,70 +1,102 @@
 import pytest
 
-
-@pytest.fixture()
-def geth_node_config(monkeypatch):
-    monkeypatch.setenv('DEPLOY_MAX_WAIT', '20')
-    monkeypatch.setenv('DEPLOY_WAIT_FOR_BLOCK', '1')
-    monkeypatch.setenv('DEPLOY_WAIT_FOR_BLOCK_MAX_WAIT', '180')
-    # Deploy contracts using the RPC client
-    monkeypatch.setenv('DEPLOY_CLIENT_TYPE', 'rpc')
-
-    monkeypatch.setenv('GETH_MAX_WAIT', '45')
-
-
-@pytest.fixture(autouse=True, scope="module")
-def contract_deployment_config(populus_config):
-    populus_config.deploy_contracts = [
-        "Scheduler",
-        "CallLib",
-    ]
-
-
-@pytest.fixture(scope="module")
-def FutureBlockCall(contracts, deployed_contracts):
-    from populus.contracts import (
-        link_contract_dependency,
-    )
-    return link_contract_dependency(
-        link_contract_dependency(contracts.FutureBlockCall, deployed_contracts.CallLib),
-        deployed_contracts.AccountingLib,
-    )
+from eth_abi import (
+    encode_abi,
+)
+from web3.utils.string import (
+    force_obj_to_bytes,
+)
+from web3.utils.abi import (
+    get_abi_input_types,
+    function_abi_to_4byte_selector,
+)
+from web3.utils.encoding import (
+    decode_hex,
+)
+from populus.utils.transactions import (
+    get_block_gas_limit,
+    get_contract_address_from_txn,
+)
 
 
 @pytest.fixture
-def deploy_future_block_call(deploy_client, FutureBlockCall, deploy_coinbase):
-    from populus.contracts import (
-        deploy_contract,
-    )
-    from populus.utils import (
-        get_contract_address_from_txn,
-    )
+def FutureBlockCall(chain):
+    return chain.get_contract_factory('FutureBlockCall')
 
-    def _deploy_future_block_call(contract_function=None, scheduler_address=None,
-                                  target_block=None, grace_period=255,
-                                  required_gas=1000000, payment=1, donation=1,
-                                  endowment=None, call_data="",
-                                  require_depth=0, call_value=0):
+
+@pytest.fixture
+def Canary(chain):
+    return chain.get_contract_factory('Canary')
+
+
+@pytest.fixture
+def CallLib(chain):
+    return chain.get_contract_factory('CallLib')
+
+
+@pytest.fixture
+def SchedulerLib(chain):
+    return chain.get_contract_factory('SchedulerLib')
+
+
+@pytest.fixture
+def deploy_fbc(chain, web3, FutureBlockCall):
+
+    def _deploy_fbc(contract=None,
+                    method_name=None,
+                    arguments=None,
+                    scheduler_address=None,
+                    target_block=None,
+                    grace_period=255,
+                    required_gas=1000000,
+                    payment=1,
+                    donation=1,
+                    endowment=None,
+                    call_data="",
+                    require_depth=0,
+                    call_value=0):
+        if arguments is not None and call_data:
+            raise ValueError("Cannot specify both arguments and call_data")
+        elif arguments is not None and not method_name:
+            raise ValueError("Method name must be provided if providing arguments")
+
         if endowment is None:
-            endowment = deploy_client.get_max_gas() * deploy_client.get_gas_price() + payment + donation + call_value
+            endowment = (
+                get_block_gas_limit(web3) *
+                web3.eth.gasPrice +
+                payment +
+                donation +
+                call_value
+            )
 
         if target_block is None:
-            target_block = deploy_client.get_block_number()
+            target_block = web3.eth.blockNumber
 
         if scheduler_address is None:
-            scheduler_address = deploy_coinbase
+            scheduler_address = web3.eth.coinbase
 
-        if contract_function is None:
-            abi_signature = ""
-            contract_address = scheduler_address
+        if contract is None:
+            contract_address = web3.eth.coinbase
         else:
-            abi_signature = contract_function.encoded_abi_signature
-            contract_address = contract_function._contract._meta.address
+            contract_address = contract.address
 
-        deploy_txn_hash = deploy_contract(
-            deploy_client,
-            FutureBlockCall,
-            constructor_args=(
+        if method_name is None:
+            abi_signature = ""
+        else:
+            method_abi = contract.find_matching_fn_abi('setBool', arguments)
+            hex_abi_signature = function_abi_to_4byte_selector(method_abi)
+            abi_signature = decode_hex(hex_abi_signature)
+
+            if not call_data and arguments:
+                arguent_types = get_abi_input_types(method_abi)
+                call_data = encode_abi(
+                    arguent_types,
+                    force_obj_to_bytes(arguments),
+                )
+
+        deploy_txn_hash = FutureBlockCall.deploy(
+            {'value': endowment},
+            [
                 scheduler_address,
                 target_block,
                 grace_period,
@@ -76,107 +108,89 @@ def deploy_future_block_call(deploy_client, FutureBlockCall, deploy_coinbase):
                 require_depth,
                 payment,
                 donation,
-            ),
-            gas=int(deploy_client.get_max_gas() * 0.95),
-            value=endowment,
+            ],
         )
 
-        call_address = get_contract_address_from_txn(deploy_client, deploy_txn_hash, 180)
-        call = FutureBlockCall(call_address, deploy_client)
-        return call
-    return _deploy_future_block_call
-
-
-@pytest.fixture(scope="module")
-def Canary(contracts):
-    return contracts.Canary
-
-
-@pytest.fixture()
-def deploy_canary_contract(deployed_contracts, deploy_client, denoms, Canary):
-    from populus.contracts import (
-        deploy_contract,
-    )
-    from populus.utils import (
-        get_contract_address_from_txn,
-    )
-    def _deploy_canary_contract(endowment=None, scheduler_address=None):
-        if endowment is None:
-            endowment = 5 * denoms.ether
-
-        if scheduler_address is None:
-            scheduler_address = deployed_contracts.Scheduler._meta.address
-
-        deploy_txn_hash = deploy_contract(
-            deploy_client,
-            Canary,
-            constructor_args=(scheduler_address,),
-            gas=int(deploy_client.get_max_gas() * 0.95),
-            value=endowment,
+        contract_address = get_contract_address_from_txn(
+            web3, deploy_txn_hash, 180,
         )
-
-        canary_address = get_contract_address_from_txn(deploy_client, deploy_txn_hash, 180)
-        canary = Canary(canary_address, deploy_client)
-        return canary
-    return _deploy_canary_contract
-
-
-@pytest.fixture()
-def canary(deploy_canary_contract):
-    return deploy_canary_contract()
-
-
-@pytest.fixture(scope="module")
-def CallLib(deployed_contracts):
-    return deployed_contracts.CallLib
-
-
-@pytest.fixture(scope="module")
-def SchedulerLib(deployed_contracts):
-    return deployed_contracts.SchedulerLib
-
-
-@pytest.fixture(scope="session")
-def denoms():
-    from ethereum.utils import denoms as ether_denoms
-    return ether_denoms
-
-
-@pytest.fixture(scope="module")
-def get_call(SchedulerLib, FutureBlockCall, deploy_client):
-    def _get_call(txn_hash):
-        call_scheduled_logs = SchedulerLib.CallScheduled.get_transaction_logs(txn_hash)
-        if not len(call_scheduled_logs):
-            call_rejected_logs = SchedulerLib.CallRejected.get_transaction_logs(txn_hash)
-            if len(call_rejected_logs):
-                reject_data = SchedulerLib.CallRejected.get_log_data(call_rejected_logs[0])
-                raise ValueError("CallRejected: {0}".format(reject_data))
-            raise ValueError("No scheduled call found")
-        call_scheduled_data = SchedulerLib.CallScheduled.get_log_data(call_scheduled_logs[0])
-
-        call_address = call_scheduled_data['call_address']
-        call = FutureBlockCall(call_address, deploy_client)
+        call = FutureBlockCall(address=contract_address)
         return call
-    return _get_call
+    return _deploy_fbc
 
 
-@pytest.fixture(scope="module")
-def get_call_rejection_data(SchedulerLib):
-    def _get_rejection_data(txn_hash):
-        rejection_logs = SchedulerLib.CallRejected.get_transaction_logs(txn_hash)
-        assert len(rejection_logs) == 1
-        rejection_data = SchedulerLib.CallRejected.get_log_data(rejection_logs[0])
-
-        return rejection_data
-    return _get_rejection_data
-
-
-@pytest.fixture(scope="module")
-def get_execution_data(CallLib):
-    def _get_execution_data(txn_hash):
-        execution_logs = CallLib.CallExecuted.get_transaction_logs(txn_hash)
-        assert len(execution_logs) == 1
-        execution_data = CallLib.CallExecuted.get_log_data(execution_logs[0])
-
-        return execution_data
-    return _get_execution_data
+#
+# TODO: rework this.
+#
+#@pytest.fixture()
+#def deploy_canary_contract(deployed_contracts, deploy_client, denoms, Canary):
+#    from populus.contracts import (
+#        deploy_contract,
+#    )
+#    from populus.utils import (
+#        get_contract_address_from_txn,
+#    )
+#    def _deploy_canary_contract(endowment=None, scheduler_address=None):
+#        if endowment is None:
+#            endowment = 5 * denoms.ether
+#
+#        if scheduler_address is None:
+#            scheduler_address = deployed_contracts.Scheduler._meta.address
+#
+#        deploy_txn_hash = deploy_contract(
+#            deploy_client,
+#            Canary,
+#            constructor_args=(scheduler_address,),
+#            gas=int(deploy_client.get_max_gas() * 0.95),
+#            value=endowment,
+#        )
+#
+#        canary_address = get_contract_address_from_txn(deploy_client, deploy_txn_hash, 180)
+#        canary = Canary(canary_address, deploy_client)
+#        return canary
+#    return _deploy_canary_contract
+#
+#
+#@pytest.fixture()
+#def canary(deploy_canary_contract):
+#    return deploy_canary_contract()
+#
+#
+#@pytest.fixture(scope="module")
+#def get_call(SchedulerLib, FutureBlockCall, deploy_client):
+#    def _get_call(txn_hash):
+#        call_scheduled_logs = SchedulerLib.CallScheduled.get_transaction_logs(txn_hash)
+#        if not len(call_scheduled_logs):
+#            call_rejected_logs = SchedulerLib.CallRejected.get_transaction_logs(txn_hash)
+#            if len(call_rejected_logs):
+#                reject_data = SchedulerLib.CallRejected.get_log_data(call_rejected_logs[0])
+#                raise ValueError("CallRejected: {0}".format(reject_data))
+#            raise ValueError("No scheduled call found")
+#        call_scheduled_data = SchedulerLib.CallScheduled.get_log_data(call_scheduled_logs[0])
+#
+#        call_address = call_scheduled_data['call_address']
+#        call = FutureBlockCall(call_address, deploy_client)
+#        return call
+#    return _get_call
+#
+#
+#@pytest.fixture(scope="module")
+#def get_call_rejection_data(SchedulerLib):
+#    def _get_rejection_data(txn_hash):
+#        rejection_logs = SchedulerLib.CallRejected.get_transaction_logs(txn_hash)
+#        assert len(rejection_logs) == 1
+#        rejection_data = SchedulerLib.CallRejected.get_log_data(rejection_logs[0])
+#
+#        return rejection_data
+#    return _get_rejection_data
+#
+#
+#@pytest.fixture(scope="module")
+#def get_execution_data(CallLib):
+#    def _get_execution_data(txn_hash):
+#        execution_logs = CallLib.CallExecuted.get_transaction_logs(txn_hash)
+#        assert len(execution_logs) == 1
+#        execution_data = CallLib.CallExecuted.get_log_data(execution_logs[0])
+#
+#        return execution_data
+#    return _get_execution_data
--- tests/accounting/test_call_payout.py
+++ tests/accounting/test_call_payout.py
@@ -1,42 +1,81 @@
-deploy_contracts = [
-    "CallLib",
-    "Scheduler",
-    "TestCallExecution",
-]
+from eth_abi import (
+    decode_abi,
+    decode_single,
+)
+from web3.utils.abi import (
+    get_abi_input_types,
+    get_abi_input_names,
+    get_indexed_event_inputs,
+    exclude_indexed_event_inputs,
+)
+from populus.utils.transactions import (
+    wait_for_transaction_receipt,
+)
 
 
-def test_execution_payment(deploy_client, deployed_contracts,
-                           deploy_future_block_call, denoms,
-                           FutureBlockCall, CallLib, SchedulerLib):
-    scheduler = deployed_contracts.Scheduler
-    client_contract = deployed_contracts.TestCallExecution
+def get_event_data(event_abi, log_entry):
+    log_topics = log_entry['topics'][1:]
+    log_topics_abi = get_indexed_event_inputs(event_abi)
+    log_topic_types = get_abi_input_types({'inputs': log_topics_abi})
+    log_topic_names = get_abi_input_names({'inputs': log_topics_abi})
 
-    call = deploy_future_block_call(
-        client_contract.setBool,
-        target_block=deploy_client.get_block_number() + 1000,
+    log_data = log_entry['data']
+    log_data_abi = exclude_indexed_event_inputs(event_abi)
+    log_data_types = get_abi_input_types({'inputs': log_data_abi})
+    log_data_names = get_abi_input_names({'inputs': log_data_abi})
+
+    decoded_log_data = decode_abi(log_data_types, log_data)
+    decoded_topic_data = [
+        decode_single(topic_type, topic_data)
+        for topic_type, topic_data
+        in zip(log_topic_types, log_topics)
+    ]
+
+    decoded_data = {
+        'topics': dict(zip(log_topic_names, decoded_topic_data)),
+        'data': dict(zip(log_data_names, decoded_log_data)),
+    }
+    return decoded_data
+
+
+#def test_execution_payment(deploy_client, deployed_contracts,
+#                           deploy_future_block_call, denoms,
+#                           FutureBlockCall, CallLib, SchedulerLib):
+def test_execution_payment(unmigrated_chain, web3, FutureBlockCall, CallLib,
+                           deploy_fbc):
+    chain = unmigrated_chain
+    client_contract = chain.get_contract('TestCallExecution')
+
+    call = deploy_fbc(
+        contract=client_contract,
+        method_name='setBool',
+        target_block=web3.eth.blockNumber + 20,
         payment=12345,
         donation=54321,
     )
 
-    deploy_client.wait_for_block(call.targetBlock())
+    while web3.eth.blockNumber < call.call().targetBlock():
+        web3._requestManager.request_blocking("evm_mine", [])
 
-    assert client_contract.get_balance() == 0
+    assert web3.eth.getBalance(client_contract.address) == 0
 
-    assert client_contract.v_bool() is False
-    assert client_contract.wasSuccessful() == 0
+    assert client_contract.call().v_bool() is False
+    assert client_contract.call().wasSuccessful() == 0
 
-    call_txn_hash = client_contract.doExecution(call._meta.address)
-    call_txn = deploy_client.get_transaction_by_hash(call_txn_hash)
-    call_txn_receipt = deploy_client.wait_for_transaction(call_txn_hash)
+    call_txn_hash = client_contract.transact().doExecution(call.address)
+    call_txn_receipt = wait_for_transaction_receipt(web3, call_txn_hash)
 
-    assert client_contract.wasSuccessful() == 1
-    assert client_contract.v_bool() is True
+    assert client_contract.call().wasSuccessful() == 1
+    assert client_contract.call().v_bool() is True
 
-    execute_logs = CallLib.CallExecuted.get_transaction_logs(call_txn_hash)
-    assert len(execute_logs) == 1
-    execute_data = CallLib.CallExecuted.get_log_data(execute_logs[0])
+    log_entries = call_txn_receipt['logs']
+    assert log_entries
 
-    expected_payout = 12345 + execute_data['gasCost']
+    event_abi = CallLib.find_matching_event_abi('CallExecuted')
 
-    assert execute_data['payment'] == expected_payout
-    assert client_contract.get_balance() == execute_data['payment']
+    event_data = get_event_data(event_abi, call_txn_receipt['logs'][0])
+
+    expected_payout = 12345 + event_data['data']['gasCost']
+
+    assert event_data['data']['payment'] == expected_payout
+    assert web3.eth.getBalance(client_contract.address) == event_data['data']['payment']

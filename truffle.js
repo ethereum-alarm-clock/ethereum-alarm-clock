089936fb9f10e19c24e8cf3f5dba231c253787b7
Piotr Kosiński <piotrkosinski@gmail.com>
2018-02-26 22:43:38 +0100

    Set the example gasPrice in truffle script

--- truffle.js
+++ truffle.js
@@ -2,6 +2,7 @@ module.exports = {
     networks: {
         development: {
             gas: 4700000,
+            gasPrice: 10000000000, // 10 gwei
             host: "localhost",
             port: 8545,
             network_id: "*" // Match any network id

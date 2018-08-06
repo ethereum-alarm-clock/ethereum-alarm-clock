d4b042c13fada36de97829c3e791dc610fef9683
Logan Saether <lsaether@protonmail.com>
2018-08-06 18:33:23 +0300

    new assets (#156)

--- truffle.js
+++ truffle.js
@@ -1,3 +1,8 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+// 0 - 0xD593A23b099e85AE97CAB1b5a645959211B03277
+const mnemonic = "";
+
 module.exports = {
   networks: {
     development: {
@@ -17,9 +22,14 @@ module.exports = {
     ropsten: {
       gas: 4700000,
       gasPrice: 10000000000, // 10 gwei
-      host: "localhost",
-      port: 8545,
+      provider: () => new HDWalletProvider(mnemonic, "https://ropsten.infura.io"),
       network_id: "3"
+    },
+    rinkeby: {
+      gas: 4700000,
+      gasPrice: 20000000000, // 20 gwei
+      provider: () => new HDWalletProvider(mnemonic, "https://rinkeby.infura.io"),
+      network_id: "4"
     }
   }
 };
